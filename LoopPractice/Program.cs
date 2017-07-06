8d0b0f38edffa47cec4ceb4466f139f391223181
pfittante <pfittante@yahoo.com>
2017-07-06 10:14:55 -0400

    Loop practice update

--- LoopPractice/Program.cs
+++ LoopPractice/Program.cs
@@ -10,6 +10,62 @@ namespace LoopPractice
     {
         static void Main(string[] args)
         {
+
+        //    string[] weekDays = new string[7];
+
+        //    for (int i = 0; i < weekDays.Length; i++)
+        //    {
+
+        //        Console.Write("Please enter a day of the week: ");
+        //        weekDays[i] = Console.ReadLine();
+
+        //    }
+
+        //    Array.Reverse(weekDays);
+
+        //    for (int i = 0; i < weekDays.Length; i++)
+        //    {
+        //        Console.Write(weekDays[i]);
+        //    }
+
+
+            int min = 1;
+            int max = 50;
+            bool exitBool = false;
+            string exitStr;
+            int[] luckyNum = new int[50];
+            Random randNum = new Random();
+
+            while (exitBool == false)
+            {
+
+                for (int i = 0; i < luckyNum.Length; i++)
+                {
+                luckyNum[i] = randNum.Next(min, max);
+                }
+
+                for (int i = 0; i < luckyNum.Length; i++)
+                {
+                    if (luckyNum[i] == 3 || luckyNum[i] == 5 || luckyNum[i] == 7 || luckyNum[i] == 11 || luckyNum[i] == 15)
+                    {
+                        Console.WriteLine(luckyNum[i]);
+                    }
+                }
+
+                Console.Write("Would you like to try again? If yes press enter and if not enter \"quit\": ");
+                exitStr = Console.ReadLine();
+
+                if (exitStr == "quit")
+                {
+                    exitBool = true;
+                }
+                else
+                {
+                    exitBool = false;
+                }
+                
+            }
+
         }
     }
 }
